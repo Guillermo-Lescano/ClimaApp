fc2d8aec95a2653b9736991b7575dbfd4a333b85
Guillermo Lescano <glescano@vates.com>
2023-08-11 01:29:36 -0300

    Creando Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+   <div className='contenedor'>
+    <form action="">
+      <div className='campo'>
+        <label htmlFor="ciudad">Ciudad</label>
+        <input type="text" id='ciudad' name='ciudad' />
+      </div>
+      <div className='campo'>
+        <label htmlFor="ciudad">Ciudad</label>
+        <select
+          id='pais'
+          name='pais'>
+            <option value="">Seleccione un pais</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">Mexico</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica </option>
+            <option value="ES">España</option>
+            <option value="PE">Peru</option>
+        </select>
+      </div>
+      <input type="submit" value='Consulta Clima'/>
+    </form>
+   </div>
+   
   )
 }
 
